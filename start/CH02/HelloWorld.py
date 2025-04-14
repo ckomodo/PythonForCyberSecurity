0180cb2431bed9a73df866306d3be57f54de2389
ckomodo <chrissakwa@gmail.com>
2025-04-14 11:40:50 -0700

    Added conditional

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
 # Created 
-print ("Hello there and Welcome")
-user_name = input("Please enter your name ")
-print("Hello {0}, " .format(user_name), "today will be a good day")
-age = float(input("What year were you born?"))
-next_age = 2027 - age
-print ("In two years you will be {0}" .format(next_age))
+#print ("Hello there and Welcome")
+#user_name = input("Please enter your name ")
+#print("Hello {0}, " .format(user_name), "today will be a good day")
+#age = float(input("What year were you born?"))
+#next_age = 2027 - age
+#print ("In two years you will be {0}" .format(next_age))
+a = 2
+b = 3
+print ("a" + "b")
+#a =2 
+#b = 3
+#c = 4
+#print ("a" + b)
+#print('Hello', end=' ')
+#print('There', end='')
--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # example workign with conditionals
 #By 
+
+opinion = input ("Is today a good day? y/n ")
+if opinion == "y":
+    print("Yes it is")
+else: 
+    print ("Call Ice Cube")
