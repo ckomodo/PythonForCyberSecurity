f48e3580bf3f1cf89fdee0c94bd9cc0a8f61aa11
ckomodo <chrissakwa@gmail.com>
2025-04-13 22:11:03 -0700

    user age

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
 # Created 
-print ("Hello user, please enter your name")
-
+print ("Hello there and Welcome")
+user_name = input("Please enter your name ")
+print("Hello {0}, " .format(user_name), "today will be a good day")
+age = float(input("What year were you born?"))
+next_age = 2027 - age
+print ("In two years you will be {0}" .format(next_age))
