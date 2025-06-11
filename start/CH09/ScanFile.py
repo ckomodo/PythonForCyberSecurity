13ae26a89367832043d4e209db342541950c7126
ckomodo <chrissakwa@gmail.com>
2025-06-11 13:45:09 -0700

    all changes before tkinter homework

--- start/CH09/ScanFile.py
+++ start/CH09/ScanFile.py
@@ -4,8 +4,18 @@
 # By CW
 
 #import python modules
+import requests
+import configparser
+import hashlib
+
 
 #function to get API key
+def get_api_key(key_name):
+    config = configparser.ConfigParser()
+    config.read("c:\\users\\student\\secrets.ini")
+    api_key = config ["APIKeys"][key_name]
+    return api_key
+    
 
 #function to get VT report
 
@@ -14,13 +24,17 @@
 #main body of code - part of it from virus total
 
 #Get API key
+token = get_api_key("VirusTotal")
 
 #Prompt user for the file/file pathway
+target_file = input("File to scan ")
 
 #Get file hash
+
 
 #Get VT report
 
 #IF report is not there, upload VT file
     #wait
     #get VT report
+    
---
+++ start/CH11/tkinterGUI.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import tkinter
+m = tkinter.Tk()
+'''
+widgets are added here
+'''
+m.mainloop()
+
